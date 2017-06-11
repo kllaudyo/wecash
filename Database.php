6547cbb35e75bbb011dd66d1050463362144d75d
Claudio Acioli <kllaudyo@gmail.com>
2017-06-11 12:48:04 -0300

    Adicionado arquivos principais de configuracao

---
+++ Database.php
@@ -0,0 +1,10 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: claudio
+     * Date: 11/06/17
+     * Time: 12:00
+     */
+
+    $database_connection = new PDO("sqlite:wecash.db");
---
+++ Settings.php
@@ -0,0 +1,28 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: claudio
+     * Date: 11/06/17
+     * Time: 12:12
+     */
+
+    function debug($resource)
+    {
+        print "<pre>";
+        print_r($resource);
+        print "</pre>";
+    }
+
+    function import($class_name)
+    {
+        include($class_name.".php");
+    }
+
+    ini_set('display_errors', 1);
+
+    ini_set('display_startup_errors', 1);
+
+    error_reporting(E_ALL);
+
+    spl_autoload_register("import");
